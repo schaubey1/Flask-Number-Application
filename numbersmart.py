fa12ce87921f0308e9f4dfaf9ec6a1e54c3c7651
schaubey1 <44815510+schaubey1@users.noreply.github.com>
2020-09-03 23:54:23 -0700

    Update numbersmart.py
    -Added unoptimized sieves algorithm for finding primes.
    -Changed while and for loops to one liners.
    -Created strings for messages.

--- numbersmart.py
+++ numbersmart.py
@@ -1,68 +1,50 @@
 from flask import Flask
 from flask import jsonify
+
 app = Flask(__name__)
+
+boundsErr = "The number you entered is out of bounds."
+welcomeMsg = "Welcome! Enter the url/int to see your number."
+
 
 @app.route('/')
 def index():
-	return 'Welcome! Use URL: http://127.0.0.1:5000/! Enter the url/int to see your number. '
+    return jsonify(welcomeMsg)
 
-@app.route('/<int:number>',methods=['GET'])		#display integer method
+
+@app.route('/<int:number>', methods=['GET'])
 def displayIntegers(number):
-	if number>0:
-		output=list()
-		i=1
-		while i<number+1: 			#loop through and append numbers for printing.
-			output.append(i)
-			i+=1
-		return jsonify(output)
-	else: 
-		return 'Please try again with a higher number.'
+    if number > 0:
+        return jsonify([x for x in range(1, number + 1)])
+    else:
+        return jsonify(boundsErr)
 
-@app.route('/<int:number>/odd',methods=['GET'])
-def displayOdd(number):					#display odds method
-	print(number)
-	output=list()
-	if number>1:
-		i=0					#loop through and append numbers for printing.
-		while i<number+1:
-			if i% 2 != 0:			#only add odd numbers to our list.
-				output.append(i)
-			i+=1
-		return jsonify(output)
-	else:
-		return "You entered: 1. Please try a higher number."
 
-@app.route('/<int:number>/even',methods=['GET'])
-def displayEven(number):				#display even numbers method.
-	if number>1:
-		output=list()				#init list
-		i=0					#init counter, start at 0 since 0 is even.
-		while i<number+1:
-			if i% 2 == 0:
-				output.append(i)
-			i+=1
-	return jsonify(output)
+@app.route('/<int:number>/odd', methods=['GET'])
+def displayOdd(number):
+    if number > 1:
+        return jsonify([x for x in range(1, number + 1) if x % 2 != 0])
+    else:
+        return jsonify(boundsErr)
 
-@app.route('/<int:number>/prime',methods=['GET'])
+
+@app.route('/<int:number>/even', methods=['GET'])
+def displayEven(number):
+    if number > 1:
+        return jsonify([x for x in range(1, number + 1) if x % 2 == 0])
+    else:
+        return jsonify(boundsErr)
+
+
+@app.route('/<int:number>/prime', methods=['GET'])
 def displayPrime(number):
-	output=list()					#init list
-	i=2						#2 is the first prime number, set our counter to this.
-	if number>1:
-		for x in range(i,number+1):		#loop through entire arange, set prime flag as true by default.
-			prime=True			
-			for i in range(2,x):		#Determine which numbers are not prime.
-				if x%i==0:		#if cursor is divisble by number with no remainder, it is not prime.
-					prime=False
-			if prime==True:			#append and print
-				output.append(x)
-	else: 
-		return 'Number too small.'
-	return jsonify(output)
+    primeList = []
+    for i in range(2, number + 1):
+        if i not in primeList:
+            for j in range(i * i, number + 1, i):
+                primeList.append(j)
+    return jsonify(primeList)
 
-			
 
 if __name__ == "__main__":
-    app.run(host ='0.0.0.0', port = 5000, debug = False)
-
-
-
+    app.run(host='0.0.0.0', port=5002, debug=False)
