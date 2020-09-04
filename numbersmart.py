3417d20d23249de839da6a102412f7e87ccc3ca9
schaubey1 <44815510+schaubey1@users.noreply.github.com>
2020-09-04 00:59:06 -0700

    Update numbersmart.py
    - Delinted
    - Working Prime update

--- numbersmart.py
+++ numbersmart.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import jsonify
-
+import math
 app = Flask(__name__)
 
 boundsErr = "The number you entered is out of bounds."
@@ -31,7 +31,7 @@ def displayOdd(number):
 @app.route('/<int:number>/even', methods=['GET'])
 def displayEven(number):
     if number > 1:
-        return jsonify([x for x in range(1, number + 1) if x % 2 == 0])
+        return jsonify([x for x in range(0, number + 1) if x % 2 == 0])
     else:
         return jsonify(boundsErr)
 
@@ -40,11 +40,16 @@ def displayEven(number):
 def displayPrime(number):
     primeList = []
     for i in range(2, number + 1):
-        if i not in primeList:
-            for j in range(i * i, number + 1, i):
-                primeList.append(j)
+        primeList.append(i)
+    i = 2
+    while (i <= int(math.sqrt(number))):
+        if i in primeList:
+            for j in range(i * 2, number + 1, i):
+                if j in primeList:
+                    primeList.remove(j)
+        i += 1
     return jsonify(primeList)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5002, debug=False)
+    app.run(host='0.0.0.0', port=56, debug=False)
