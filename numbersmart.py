783405d16d60766361adc89f84c7e6faf6e7f518
schaubey1 <44815510+schaubey1@users.noreply.github.com>
2020-09-04 18:05:25 -0700

    Update numbersmart.py
    - Changed from camelcase to proper formatting using PyCharm.
    - Improved code based on feedback.
    - Reformatted the code using pep8.
    - Produces clean output using pycodestyle --show-source --show-pep8

--- numbersmart.py
+++ numbersmart.py
@@ -1,68 +1,76 @@
+import time
+
 from flask import Flask
 from flask import jsonify
-import time
+
 app = Flask(__name__)
 
-boundsErr = "The number you entered is out of bounds."
-welcomeMsg = "Welcome! Enter the url/int to see your number."
+bounds_err = "The number you entered is out of bounds."
+welcome_msg = "Welcome! Enter the url/int to see your number."
 
 
 @app.route('/')
 def index():
-    return jsonify(welcomeMsg)
+    return jsonify(welcome_msg)
 
 
 @app.route('/<int:number>', methods=['GET'])
-def displayIntegers(number):
+def display_integers(number):
     if number > 0:
-        return jsonify([x for x in range(1, number + 1)])
+        return jsonify(result=list(range(1, number + 1)))
     else:
-        return jsonify(boundsErr)
+        return jsonify(bounds_err)
 
 
 @app.route('/<int:number>/odd', methods=['GET'])
-def displayOdd(number):
+def display_odd(number):
     if number > 1:
-        return jsonify([x for x in range(1, number + 1) if x % 2 != 0])
+        result = list(range(1, number + 1))
+        result = result[0::2]
+        return jsonify(result)
     else:
-        return jsonify(boundsErr)
+        return jsonify(bounds_err)
 
 
 @app.route('/<int:number>/even', methods=['GET'])
-def displayEven(number):
+def display_even(number):
     if number > 1:
-        return jsonify([x for x in range(0, number + 1) if x % 2 == 0])
+        result = list(range(0, number + 1))
+        result = result[0::2]
+        return jsonify(result)
     else:
-        return jsonify(boundsErr)
+        return jsonify(bounds_err)
 
 
 @app.route('/<int:number>/prime', methods=['GET'])
-def displayPrime(number):
+def display_prime(number):
     # Create a boolean array "prime[0..n]" and
     # # initialize all entries it as true. A value
     # # in prime[i] will finally be false if i is
     # # Not a prime, else true.
-    intVal = int(number)
-    prime = [True for i in range(intVal + 1)]
-    primeIndex = 2
+    if number <= 1:
+        return jsonify(bounds_err)
+
+    prime = [True] * int(number + 1)
+    prime_index = 2
     t0 = time.time()
-    while (primeIndex * primeIndex <= intVal):
+    while prime_index * prime_index <= number + 1:
         # If prime[primeIndex] is not changed, then it is
         # a prime
-        if (prime[primeIndex]):
+        if prime[prime_index]:
             # Update all multiples of prime index.
-            for i in range(primeIndex * primeIndex, intVal + 1, primeIndex):
+            for i in range(prime_index * prime_index, number + 1, prime_index):
                 prime[i] = False
-        primeIndex += 1
+        prime_index += 1
     # add all prime numbers to the list to be printed
-    primeList = []
-    for index in range(2, intVal):
-        if prime[index]:
-            primeList.append(str(index))
+    prime_list = []
+    for iterator in range(2, number + 1):
+        if prime[iterator]:
+            prime_list.append(str(iterator))
     t1 = time.time()
-    print("Execution time:", t1-t0)
-    return jsonify(primeList)
+    print("Execution time:", t1 - t0)
+    return jsonify(prime_list)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8004, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)
